c4551677d3a61e7bff9bbe27481ee22a474915eb
kim946509 <kim946509@gmail.com>
2023-11-09 18:42:21 +0900

    총 당첨금 계산하도록 Controller에 추가

--- Lotto/src/main/kotlin/LottoController.kt
+++ Lotto/src/main/kotlin/LottoController.kt
@@ -1,12 +1,13 @@
 class LottoController {
     fun process() {
-        val money = View().inputMoney() // View에게 금액을 입력받도록 명령
+        val money = inputMoney() // View에게 금액을 입력받도록 명령
         val host = Host()
         val person = Person(money)
         buyLottos(person) // Person이 Lottos를 구매하도록 명령
-        View().printLotto(person.lottos) // Person이 Lottos를 출력하도록 명령
+        printLotto(person.lottos) // Person이 Lottos를 출력하도록 명령
         processLotto(host, person.lottos) // Host가 Lottos를 체크하도록 명령
-        printResult(host) // Host가 결과를 출력하도록 명령
+        host.calculateWinnigs() // Host가 총 당첨금을 계산하도록 명령
+        result(host) // 결과를 출력
     }
     fun buyLottos(person: Person) {
         person.buyLotto()
@@ -14,7 +15,7 @@ class LottoController {
     fun processLotto(host: Host, lottos: Lottos) {
         lottos.list.forEach { lotto: Lotto -> host.countRank(host.checkLotto(lotto)) }
     }
-    fun printResult(host: Host) {
-        View().printResult(host)
+    fun result(host: Host) {
+        printResult(host)
     }
 }
---
+++ Lotto/src/main/kotlin/LottoView.kt
@@ -0,0 +1,23 @@
+
+fun inputMoney(): Int {
+    print("로또를 구매할 금액을 입력해 주세요 : ")
+    val money = readln().toInt()
+    return money
+}
+
+fun printLotto(lottos: Lottos) {
+    println("사용자가 구매 한 로또 번호는 다음과 같습니다.")
+    lottos.list.forEachIndexed { index, lotto ->
+        println("${index + 1} : " + lotto.lottoNumber.joinToString(" "))
+    }
+}
+
+fun printResult(host: Host) {
+    println("--------------------")
+    println("당첨 결과")
+    host.totalRank.forEach { (rank, count) ->
+        println("$rank : ${rank.money}KW -> ${count}개")
+    }
+    println("--------------------")
+    println("총 당첨금 : ${host.winnigs}KW")
+}
