fbc45f1a60626702f43036ba88d219dc7c4fa619
kim946509 <kim946509@gmail.com>
2023-11-20 18:54:26 +0900

    User domain 작성

--- LottoWeb/src/main/kotlin/com/example/lottoweb/domain/Lotto.kt
+++ LottoWeb/src/main/kotlin/com/example/lottoweb/domain/Lotto.kt
@@ -1,11 +1,12 @@
 package com.example.lottoweb.domain
 
 import jakarta.persistence.*
+import lombok.Builder
 import lombok.Getter
 
 @Entity
 @Getter
-class Lotto(
+data class Lotto(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
@@ -27,22 +28,7 @@ class Lotto(
 
     @Column(nullable = false, unique = true)
     val number6: Int,
+
 ) {
-    // Builder 작성
     @Builder
-    constructor(
-        number1: Int,
-        number2: Int,
-        number3: Int,
-        number4: Int,
-        number5: Int,
-        number6: Int,
-    ) : this(
-        number1 = number1,
-        number2 = number2,
-        number3 = number3,
-        number4 = number4,
-        number5 = number5,
-        number6 = number6,
-    )
 }
