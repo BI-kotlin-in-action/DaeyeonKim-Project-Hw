0e44f9445d6d5056e23d469ff9459ea512131336
kim946509 <kim946509@gmail.com>
2023-11-28 20:52:29 +0900

    BankController Entity로 데이터 감쌈

--- LottoWeb/src/main/kotlin/com/example/lottoweb/Controller/BankController.kt
+++ LottoWeb/src/main/kotlin/com/example/lottoweb/Controller/BankController.kt
@@ -3,11 +3,12 @@ package com.example.lottoweb.Controller
 import com.example.lottoweb.dto.UserMoneyDTO
 import com.example.lottoweb.service.BankService
 import com.example.lottoweb.service.UserService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 // @Tag(name = "Bank", description = "뱅킹 관련 API")
@@ -18,11 +19,20 @@ class BankController(
     private val userService: UserService,
 ) {
     @PatchMapping("/deposit")
-    fun depositMoney(@RequestBody userMoneyDTO: UserMoneyDTO) = bankService.depositMoney(userService.findUserById(userMoneyDTO.userId), userMoneyDTO.money)
+    fun depositMoney(@RequestBody userMoneyDTO: UserMoneyDTO): ResponseEntity<String> {
+        bankService.depositMoney(userService.findUserById(userMoneyDTO.userId), userMoneyDTO.money)
+        return ResponseEntity.ok("입금 완료")
+    }
 
     @PatchMapping("/withdraw")
-    fun withdrawMoney(@RequestBody userMoneyDTO: UserMoneyDTO) = bankService.withdrawMoney(userService.findUserById(userMoneyDTO.userId), userMoneyDTO.money)
+    fun withdrawMoney(@RequestBody userMoneyDTO: UserMoneyDTO): ResponseEntity<String> {
+        bankService.withdrawMoney(userService.findUserById(userMoneyDTO.userId), userMoneyDTO.money)
+        return ResponseEntity.ok("출금 완료")
+    }
 
-    @GetMapping("/balance?userId={userId}")
-    fun getBalance(@PathVariable userId: Long) = bankService.getBalance(userService.findUserById(userId))
+    @GetMapping("/balance")
+    fun getBalance(@RequestParam id: Long): ResponseEntity<Int> {
+        val balance = bankService.getBalance(userService.findUserById(id))
+        return ResponseEntity.ok(balance)
+    }
 }
